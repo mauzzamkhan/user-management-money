4dccd54f0c7f85ced76e692fb0fe0c3a1474afff
mauzzamkhan <mauzzamkhan32@gmail.com>
2025-07-23 19:42:04 +0530

    fixed route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import UserList from "./pages/UserList/Index";
 import AddUser from "./pages/AddUser/Index";
